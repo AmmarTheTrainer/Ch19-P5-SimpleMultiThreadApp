d0c51a09741095037c35da0aa413bc87c4210f54
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-30 14:34:26 +0500

    final code update from head office

---
+++ Ch19-P5-SimpleMultiThreadApp/Printer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ch19_P5_SimpleMultiThreadApp
+{
+    class Printer
+    {
+        public void PrintNumbers()
+        {
+            // Display Thread info.
+            Console.WriteLine("-> {0} is executing PrintNumbers()",
+                                        Thread.CurrentThread.Name);
+            // Print out numbers.
+            Console.Write("Your numbers: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0}, ", i);
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(" Im just getting free... ");
+            Console.WriteLine();
+        }
+    }
+}
